90757e932d4dd52e2eeeb027bc909bc85ba2742e
Jay Thomason <jay@covariant.ai>
2020-11-29 10:08:25 -0800

    simple infinite loop boot sector

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn generate_bootloader() {
+    let input = "src/boot_sect_simple.asm";
+    let output = format!("{}/{}", std::env::var("OUT_DIR").unwrap(), "bootloader.bin");
+    println!("cargo:rerun-if-changed={}", input);
+    let nasm_output = std::process::Command::new("nasm")
+        .args(&["-fbin", input, "-o", &output])
+        .output()
+        .expect("Failed to generate bootloader");
+    assert!(nasm_output.status.success(), format!("\n\n{}\n", std::str::from_utf8(&nasm_output.stderr).unwrap()));
+}
+
+fn main() {
+    generate_bootloader();
+}
